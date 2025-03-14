185472a34ffcdae5878f9fca9a5bbd79d3173356
Eri <eri@nijika.dev>
2025-03-14 19:58:57 +0100

    titledb downloader

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,55 @@ mod router;
 mod titledb;
 
 use db::init_database;
+use futures_util::StreamExt;
+use indicatif::{ProgressBar, ProgressStyle};
+use reqwest::Client;
+use reqwest::header::ACCEPT_LANGUAGE;
 use router::create_router;
+use std::cmp::min;
+use std::fs::File;
+use std::io::Write;
 use surrealdb::Surreal;
 use surrealdb::engine::local::RocksDb;
 use titledb::TitleDBImport;
+
+async fn download_titledb(client: &Client, region: &str, language: &str) -> Result<(), String> {
+    let url = format!(
+        "https://github.com/blawar/titledb/raw/refs/heads/master/{}.{}.json",
+        region, language
+    );
+    let path = format!("{}.{}.json", region, language);
+    let res = client
+        .get(&url)
+        .send()
+        .await
+        .or(Err(format!("Failed to GET from '{}'", &url)))?;
+    let total_size = res
+        .content_length()
+        .ok_or(format!("Failed to get content length from '{}'", &url))?;
+
+    let pb = ProgressBar::new(total_size);
+    pb.set_style(ProgressStyle::default_bar()
+        .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})").unwrap()
+        .progress_chars("#>-"));
+    pb.set_message(format!("Downloading file {}", url));
+    let path_clone = path.clone();
+    let mut file = File::create(path).or(Err(format!("Failed to create file '{}'", path_clone)))?;
+    let mut downloaded: u64 = 0;
+    let mut stream = res.bytes_stream();
+
+    while let Some(item) = stream.next().await {
+        let chunk = item.or(Err(format!("Error while downloading file")))?;
+        file.write_all(&chunk)
+            .or(Err(format!("Error while writing to file")))?;
+        let new = min(downloaded + (chunk.len() as u64), total_size);
+        downloaded = new;
+        pb.set_position(new);
+    }
+
+    pb.finish_with_message(format!("Downloaded {} to {}", url, path_clone));
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
@@ -33,7 +78,14 @@ async fn main() -> color_eyre::Result<()> {
     init_database().await?;
     tokio::spawn(async {
         tracing::info!("Importing TitleDB...");
-        let us_titledb_file = std::fs::File::open("US.en.json").unwrap();
+        let region = "US";
+        let language = "en";
+        // TODO: replace this with an actual config impl
+        let client = Client::new();
+        download_titledb(&client, region, language).await.unwrap();
+
+        let path = format!("{}.{}.json", region, language);
+        let us_titledb_file = std::fs::File::open(path).unwrap();
         // let us_titledb_file = std::fs::File::open("src/zeld.json").unwrap();
         // match TitleDBImport::from_json_reader(us_titledb_file) {
         //     Ok(us_titledb) => {
@@ -43,7 +95,12 @@ async fn main() -> color_eyre::Result<()> {
         //     },
         //     Err(e) => eprintln!("Error reading titledb: {}", e),
         // },
-        let _ = TitleDBImport::from_json_reader_streaming(us_titledb_file, "US_en").await;
+        let _ = TitleDBImport::from_json_reader_streaming(
+            us_titledb_file,
+            &format!("{}.{}.json", region, language),
+        )
+        .await;
+        std::fs::remove_file(format!("{}.{}.json", region, language)).unwrap();
     });
 
     let app = create_router();
