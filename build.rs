c2a66389b2f643b3f4268ac22cf4450490a02cec
Cappy Ishihara <cappy@cappuchino.xyz>
2025-03-23 12:09:11 +0700

    Update build script to print frontend directory for better tracking of changes

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::result::Result;
 pub fn main() -> Result<(), std::io::Error> {
     let current_dir = std::env::current_dir()?;
     let frontend_dir = current_dir.join("alu-panel");
-
+    println!("cargo::rerun-if-changed={}", frontend_dir.display());
     Command::new("pnpm")
         .arg("install")
         .current_dir(&frontend_dir)
