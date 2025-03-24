04e5d0a3beff6bc5a3d5901137b1fd3725453138
Eri <eri@nijika.dev>
2025-03-24 19:47:08 +0100

    make the frontend not be as bad as before

--- alu-panel/src/components/MetadataList.vue
+++ alu-panel/src/components/MetadataList.vue
@@ -1,75 +1,83 @@
 <template>
-    <!-- this is the background -->
+    <!-- fixed background -->
     <div
         v-if="metadata"
-        class="fixed inset-0 bg-cover bg-center bg-no-repeat blur-xl opacity-10"
+        class="fixed inset-0 h-screen bg-cover bg-center bg-no-repeat blur-xl opacity-70"
         :style="{ backgroundImage: `url(${metadata.bannerUrl})` }"
     ></div>
+
     <div class="relative z-10">
-        <br />
-        <br />
-        <br />
-        <!-- what the hell is this -->
-        <div v-if="metadata" class="container mx-auto px-4">
-            <div class="flex flex-col gap-4">
-                <div class="flex space-x-4">
-                    <h1 class="text-2xl font-bold text-white">
-                        {{ metadata.name }}
-                    </h1>
-                    <h1 class="text-2xl font-bold text-gray-600">&nbsp;</h1>
-                    <h1 class="text-2xl font-bold text-gray-600">
-                        {{ metadata.releaseDate.slice(0, 4) }}
-                    </h1>
+        <div v-if="metadata" class="w-full">
+            <div class="h-[25vh]"></div>
+
+            <div class="relative">
+                <img
+                    :src="metadata.iconUrl"
+                    :alt="metadata.name"
+                    class="absolute left-8 -top-24 w-48 h-48 object-cover rounded-lg shadow-xl z-20"
+                />
+
+                <!-- Title outside the box -->
+                <div class="absolute left-64 -top-24 z-20 text-white">
+                    <div class="flex items-center gap-4 mb-4">
+                        <h1 class="text-4xl font-bold text-white">
+                            {{ metadata.name }}
+                        </h1>
+                        <h1 class="text-4xl font-bold text-gray-300">
+                            {{ metadata.releaseDate.slice(0, 4) }}
+                        </h1>
+                    </div>
+                    <p class="text-xl">
+                        {{ metadata.publisher }} |
+
+                        {{
+                            metadata.size > 1024 * 1024 * 1024
+                                ? (
+                                      metadata.size /
+                                      (1024 * 1024 * 1024)
+                                  ).toFixed(2) + " GB"
+                                : (metadata.size / (1024 * 1024)).toFixed(2) +
+                                  " MB"
+                        }}
+                    </p>
                 </div>
-                <div class="flex gap-8">
-                    <div class="flex flex-col gap-4">
-                        <img
-                            :src="metadata.iconUrl"
-                            :alt="metadata.name"
-                            class="w-48 h-48 object-cover rounded-lg"
-                        />
+
+                <div class="absolute left-64 top-6 z-20 text-white">
+                    <p>
+                        <strong>Title ID:</strong>
+                        {{ metadata.titleId }}
+                    </p>
+                    <p>
+                        <strong>Release Date:</strong>
+                        {{
+                            new Date(
+                                metadata.releaseDate.replace(
+                                    /(\d{4})(\d{2})(\d{2})/,
+                                    "$1-$2-$3",
+                                ),
+                            ).toLocaleDateString()
+                        }}
+                    </p>
+                    <p>
+                        <strong>Categories:</strong>
+                        {{ metadata.category.join(", ") }}
+                    </p>
+                </div>
+
+                <div class="bg-gray-900 min-h-[70vh] relative pt-8 rounded-lg">
+                    <div
+                        class="absolute right-8 -top-6 h-12 w-48 overflow-visible z-20"
+                    >
                         <button
                             @click="downloadGame(metadata.titleId)"
-                            class="w-48 px-8 py-2 bg-gradient-to-r from-green-600 to-green-800 text-white rounded-lg font-semibold shadow-lg hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75 flex items-center justify-center gap-2"
+                            class="absolute inset-0 px-8 py-1 bg-gradient-to-r from-green-600 to-green-800 text-white rounded-lg font-semibold shadow-lg hover:shadow-xl focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75 flex items-center justify-center gap-2"
                         >
                             Download
                         </button>
                     </div>
-                    <div class="text-white">
-                        <p><strong>Title ID:</strong> {{ metadata.titleId }}</p>
-                        <p>
-                            <strong>Publisher:</strong> {{ metadata.publisher }}
-                        </p>
-                        <p>
-                            <strong>Release Date:</strong>
-                            {{
-                                new Date(
-                                    metadata.releaseDate.replace(
-                                        /(\d{4})(\d{2})(\d{2})/,
-                                        "$1-$2-$3",
-                                    ),
-                                ).toLocaleDateString()
-                            }}
-                        </p>
-                        <p>
-                            <strong>Size:</strong>
-                            {{
-                                metadata.size > 1024 * 1024 * 1024
-                                    ? (
-                                          metadata.size /
-                                          (1024 * 1024 * 1024)
-                                      ).toFixed(2) + " GB"
-                                    : (metadata.size / (1024 * 1024)).toFixed(
-                                          2,
-                                      ) + " MB"
-                            }}
-                        </p>
-                        <p>
-                            <strong>Categories:</strong>
-                            {{ metadata.category.join(", ") }}
-                        </p>
-                        <div class="mt-4">
-                            <hr class="border-gray-800 my-4" />
+
+                    <div class="px-8 pt-20">
+                        <div class="text-white">
                             <h2 class="text-xl font-semibold mb-2">
                                 Description
                             </h2>
